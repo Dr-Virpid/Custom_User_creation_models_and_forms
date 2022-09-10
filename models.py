7f58827fa2f988ebdabdde8d5a7e4f20762e8460
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-10 14:24:57 +0100

    Update models.py

--- models.py
+++ models.py
@@ -2,6 +2,32 @@ from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
 
 
+class CustomUserManager(BaseUserManager):
+     def create_user(self, email, password=None, first_name=None, last_name=None, department=None):
+          if not email:
+               raise ValueError("Users must have an email")
+          if not password:
+               raise ValueError("Users must have a password")
+          if password1 != password2:
+               raise ValueError("password1 and password 2 not the same")
+           
+          user = self.model(email = self.normalize_email(email)
+                            department = department
+                            first_name = first_name
+                            last_name = last_name
+                            )
+          user.set_password(password)
+          user.save(using=self._db)
+          return user
+     
+     def create_superuser(self):
+          pass
+     # continue from here next time also don't forget to add forms and email confirmation logic
+          
+                            
+
+
+
 class CustomUser(AbstractBaseUser):
      email = models.EmailField(unique=True, max_length=100)
      first_name = models.CharField(max_length=50)
